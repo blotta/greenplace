74a458ffefd245a93b5167254e22b7edf2f6b7c2
Lucas Blotta <lucas@blotta.info>
2022-04-25 21:42:58 -0300

    carrinho dinamico

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,43 +1,58 @@
 import {useState, useEffect} from 'react';
-import { Container, Grid, Typography, Paper } from '@mui/material';
+import { Container, Grid, Typography, Paper, Button } from '@mui/material';
 import { Table, TableHead, TableBody, TableRow, TableContainer, TableCell } from '@mui/material';
 import getCart from '../services/cart';
+import { getProduct } from '../services/products';
 
 export default function CartPage() {
-    const [cart, setCart] = useState({});
+    const [data, setData] = useState({cart: {}, products: [], isFetching: false});
 
     useEffect(() => {
-        let mounted = true;
-        console.log('fetching cart');
-        getCart()
-            .then(item => {
-                console.log(item);
-                if (mounted) {
-                    setCart(item);
-                }
-        })
-        return () => mounted = false;
+        const fetchData = async () => {
+            console.log('fetching data');
+            try {
+                setData({...data, isFetching: true});
+                let apiCart = await getCart();
+                const products = await Promise.all(
+                    apiCart.products.map(p => getProduct(p.productId))
+                );
+                const cp = apiCart.products.map(ap => {
+                    let deets = products.filter(p => p.id == ap.productId);
+                    deets = deets[0];
+                    const ret = {
+                        ...deets,
+                        quantity: ap.quantity
+                    }
+                    return ret;
+                });
+                apiCart.products = cp;
+                setData({...data, cart: apiCart, products: products, isFetching: false});
+                console.log(data);
+            } catch (e) {
+                console.log(e);
+                setData({...data, isFetching: false});
+            }
+        }
+        fetchData();
 
     }, [])
 
-    if (cart.products == null) {
+    if (data.isFetching || data.cart.products == null ) {
         return <h1>Loading...</h1>
     }
 
-    const columns = [
-        {field: 'product', headerName: 'Produto'},
-        {field: 'quantity', headerName: 'Qtd'},
-        {field: 'price', headerName: 'Preço'},
-    ];
+    // const rows = [
+    //     {id: 1, product: 'Jaqueta de Couro', quantity: 1, price: 79.9},
+    //     {id: 2, product: 'Camiseta GGG', quantity: 2, price: 179.9},
+    // ]
 
-    const rows = [
-        {id: 1, product: 'Jaqueta de Couro', quantity: 1, price: 79.9},
-        {id: 2, product: 'Camiseta GGG', quantity: 2, price: 179.9},
-    ]
+   const rows = data.cart.products.map(p => {
+       return ({id: p.id, title: p.title, quantity: p.quantity, price: p.price })
+   });
 
     return (
         <Container sx={{marginY: 5}}>
-            <Typography variant='h4'>Carrinho</Typography>
+            <Typography variant='h4' mb={3}>Carrinho</Typography>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={8}>
 
@@ -47,14 +62,16 @@ export default function CartPage() {
                                 <TableRow>
                                     <TableCell>Produto</TableCell>
                                     <TableCell>Quantidade</TableCell>
+                                    <TableCell>Preço Unitário</TableCell>
                                     <TableCell>Preço</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {rows.map((row) => (
+                                {data.cart.products.map((row) => (
                                     <TableRow key={row.id}>
-                                        <TableCell>{row.product}</TableCell>
+                                        <TableCell>{row.title}</TableCell>
                                         <TableCell>{row.quantity}</TableCell>
+                                        <TableCell>R$ {row.price.toFixed(2)}</TableCell>
                                         <TableCell>R$ {(row.quantity * row.price).toFixed(2)}</TableCell>
                                     </TableRow>
                                 ))}
@@ -66,7 +83,28 @@ export default function CartPage() {
 
                 <Grid item xs={12} md={4}>
 
-                    <Typography variant='body2'>Total: R$ {rows.reduce((acc, cur) => acc + (cur.quantity * cur.price), 0).toFixed(2)}</Typography>
+                    <Paper sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between'
+                    }}>
+
+                        <Typography variant='h6' align='center' sx={{padding: 2}}>
+                            Total: R$ {data.cart.products.reduce((acc, cur) => acc + (cur.quantity * cur.price), 0).toFixed(2)}
+                        </Typography>
+
+
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{borderRadius: 0}}
+                            disabled
+                            to="/cart"
+                            // onClick={() => history.push('/cart')}
+                        >
+                            Finalizar Compra
+                        </Button>
+                    </Paper>
 
                 </Grid>
             </Grid>
--- src/services/products.js
+++ src/services/products.js
@@ -2,3 +2,8 @@ export function getProducts() {
     return fetch('https://fakestoreapi.com/products')
         .then(data => data.json());
 }
+
+export function getProduct(id) {
+    return fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(data => data.json());
+}
